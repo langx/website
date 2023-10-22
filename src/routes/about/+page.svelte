d81247ddb15eac0c3a14266d3b4927f238636b91
bhc <behicsakar@gmail.com>
2023-10-22 08:48:00 +0300

    about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,15 +7,13 @@
 	<h1>About this app</h1>
 
 	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
+		Welcome to our app! We are a team of developers passionate about creating innovative solutions
+		to real-world problems. Our app is designed to help you streamline your workflow and increase
+		productivity.
 	</p>
 
-	<pre>npm create svelte@latest</pre>
-
 	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
+		We are constantly working to improve our app and add new features. If you have any feedback or
+		suggestions, please don't hesitate to contact us.
 	</p>
 </div>
