b71bb07b728bc679c2999a12ced59fc766a0e08b
bhc <behicsakar@gmail.com>
2023-10-23 21:12:16 +0300

    del style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,36 +14,3 @@
 		<p>Copyright 2023</p>
 	</footer>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
