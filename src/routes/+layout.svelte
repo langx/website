3c87592ada5a7fdab83f557f9f691a0acfbfee38
bhc <behicsakar@gmail.com>
2023-10-24 04:55:57 +0300

    del style.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,1 @@
-<script>
-	import Header from './Header.svelte';
-	import './styles.css';
-</script>
-
-<div class="app">
-	<Header />
-
-	<main>
-		<slot />
-	</main>
-
-	<footer>
-		<p>Copyright 2023</p>
-	</footer>
-</div>
+<slot />
