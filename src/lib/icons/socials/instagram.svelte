c9239a342f7d56509a34d76ab9c62a5d47985105
bhc <behicsakar@gmail.com>
2023-10-26 03:42:29 +0300

    social icons and links

---
+++ src/lib/icons/socials/instagram.svelte
@@ -0,0 +1,28 @@
+<svg
+	width="100%"
+	height="100%"
+	stroke-width="1.5"
+	viewBox="0 0 24 24"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<path
+		stroke="currentColor"
+		stroke-width="1.5"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M12 16a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z"
+	/>
+	<path
+		stroke="currentColor"
+		stroke-width="1.5"
+		d="M3 16V8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5Z"
+	/>
+	<path
+		stroke="currentColor"
+		stroke-width="1.5"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="m17.5 6.51.01-.011"
+	/>
+</svg>
