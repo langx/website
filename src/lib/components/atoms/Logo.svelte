da65d93ba2f2e1fe3c573fe136e2543ccaaf9b77
bhc <behicsakar@gmail.com>
2023-10-26 04:36:12 +0300

    logo

--- src/lib/components/atoms/Logo.svelte
+++ src/lib/components/atoms/Logo.svelte
@@ -1,22 +1,23 @@
 <script>
-	export let animated = true;
+	export let animated = false;
 </script>
 
 <div class="logo" class:animated>
 	<svg
 		id="logo-text"
-		width="723.84009"
+		width="1023.84009"
 		height="116.720268"
-		viewBox="0 0 723.84009 116.720269"
+		viewBox="0 0 1023.84009 116.720269"
 		fill="none"
 		xmlns="http://www.w3.org/2000/svg"
 	>
-		<text xml:space="preserve" id="text" transform="translate(-278.77228,-206.83728)"
-			><tspan x="275.57227" y="292.17088" id="tspan1319"
-				><tspan style="font-size:106.667px;font-family:YoungSerif;" id="tspan1317">Site Logo</tspan
-				></tspan
-			></text
-		>
+		<text xml:space="preserve" id="text" transform="translate(-278.77228,-206.83728)">
+			<tspan x="275.57227" y="292.17088" id="tspan1319">
+				<tspan style="font-size:106.667px;font-family:Comfortaa;" id="tspan1317">
+					languageXchange
+				</tspan>
+			</tspan>
+		</text>
 	</svg>
 </div>
 
