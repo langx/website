053e56a52caa95e6b88b0c3ed848680df31fc309
bhc <behicsakar@gmail.com>
2023-12-20 20:33:50 -0500

    Update footer and add privacy policy page

---
+++ src/routes/privacy-policy/+layout.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Waves from '$lib/components/organisms/Waves.svelte';
+	import Header from '$lib/components/organisms/Header.svelte';
+	import Footer from '$lib/components/organisms/Footer.svelte';
+
+	import { description, image, keywords, title, siteBaseUrl } from '$lib/data/meta';
+</script>
+
+<svelte:head>
+	<link rel="“canonical”" href={siteBaseUrl} />
+	<meta name="keywords" content={keywords.join(', ')} />
+
+	<meta name="description" content={description} />
+	<meta property="og:description" content={description} />
+	<meta name="twitter:description" content={description} />
+
+	<title>{title}</title>
+	<meta property="og:title" content={title} />
+	<meta name="twitter:title" content={title} />
+
+	<meta property="og:image" content={image} />
+	<meta name="twitter:image" content={image} />
+
+	<meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
+<Waves />
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<Footer />
