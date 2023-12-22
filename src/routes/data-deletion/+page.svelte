263551368e27a225b8aa944bd1335ecf94e317c8
bhc <behicsakar@gmail.com>
2023-12-21 20:10:30 -0500

    Add data deletion instructions and related pages

---
+++ src/routes/data-deletion/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Hero from '$lib/components/organisms/Hero.svelte';
+	import DataDeletion from '$lib/components/organisms/DataDeletion.svelte';
+</script>
+
+<div class="container">
+	<Hero />
+	<DataDeletion />
+</div>
