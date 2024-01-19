491737880379b89786247c51ea91dad4d13de0d8
bhc <behicsakar@gmail.com>
2024-01-19 04:00:09 -0500

    Remove unused BlogPost type definition

--- src/lib/utils/types.ts
+++ src/lib/utils/types.ts
@@ -21,18 +21,3 @@ export type Feature = {
 	image: string;
 	tags: TagType[];
 };
-
-export type BlogPost = {
-	tags: string[];
-	keywords: string[];
-	hidden: boolean;
-	slug: string;
-	title: string;
-	date: string;
-	updated: string;
-	excerpt: string;
-	html: string | undefined;
-	readingTime: string;
-	relatedPosts: BlogPost[];
-	coverImage: string | undefined;
-};
