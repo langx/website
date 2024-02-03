e0c445c290bff3f112ed042c6c0771609c6dcdf5
bhc <behicsakar@gmail.com>
2024-02-03 22:35:57 +0300

    Update theme colors in ThemeToggle and Header components

--- src/lib/components/molecules/ThemeToggle.svelte
+++ src/lib/components/molecules/ThemeToggle.svelte
@@ -68,14 +68,14 @@
 			#sun {
 				fill: var(--color--text);
 				> * {
-					filter: drop-shadow(0px 0px 3px var(--color--text));
+					filter: drop-shadow(0px 0px 3px var(--color--secondary));
 				}
 			}
 
 			#sun-beams {
 				stroke: var(--color--text);
 				line {
-					filter: drop-shadow(0px 0px 3px var(--color--text));
+					filter: drop-shadow(0px 0px 3px var(--color--secondary));
 				}
 			}
 		}
