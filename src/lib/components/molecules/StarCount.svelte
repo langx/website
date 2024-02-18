65502b6906b8fe7876a13ef68421ec9a790cd785
Neebooo <imnano@mailfence.com>
2024-02-18 16:49:03 +0100

    Applied prettier to StarCount.svelte

--- src/lib/components/molecules/StarCount.svelte
+++ src/lib/components/molecules/StarCount.svelte
@@ -1,37 +1,48 @@
 <script>
-	import Github from "$lib/icons/socials/github.svelte";
-	import { onMount } from "svelte";
-    
-    let starCount = -1;
+	import Github from '$lib/icons/socials/github.svelte';
+	import { onMount } from 'svelte';
 
-    onMount(async () => {
-        starCount = (await (await fetch("https://api.github.com/repos/languageXchange/languageXchange?page=$i&per_page=100")).json()).stargazers_count
-    })
+	let starCount = -1;
 
+	onMount(async () => {
+		starCount = (
+			await (
+				await fetch(
+					'https://api.github.com/repos/languageXchange/languageXchange?page=$i&per_page=100'
+				)
+			).json()
+		).stargazers_count;
+	});
 </script>
-<div class:mtop={starCount > 0} class="star-count">
-    <a class="star" target="_blank" href="https://github.com/languageXchange/languageXchange/stargazers">
-        <Github />
-    </a>
-    {#if starCount > 0}
-    <small>{starCount}</small>
-    {/if}
-</div>
-<style lang="scss">
-    .star-count {
-        .mtop {
-            margin-top: 10px;
-        }
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        small {
-            color: var(--color--text);
-        }
 
-        .star {
-            color: var(--color--text);
-            height: 24px;
-        }
-    }
+<div class:mtop={starCount > 0} class="star-count">
+	<a
+		class="star"
+		target="_blank"
+		href="https://github.com/languageXchange/languageXchange/stargazers"
+	>
+		<Github />
+	</a>
+	{#if starCount > 0}
+		<small>{starCount}</small>
+	{/if}
+</div>
+
+<style lang="scss">
+	.star-count {
+		.mtop {
+			margin-top: 10px;
+		}
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		small {
+			color: var(--color--text);
+		}
+
+		.star {
+			color: var(--color--text);
+			height: 24px;
+		}
+	}
 </style>
