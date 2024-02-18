fcc41cdd4ed838317f8ee2893d746caf97ae1abc
Neebooo <imnano@mailfence.com>
2024-02-18 16:17:43 +0100

    Add starCount at header

---
+++ src/lib/components/molecules/StarCount.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Star from "$lib/icons/star.svelte";
+	import { onMount } from "svelte";
+    
+    let starCount = -1;
+
+    onMount(async () => {
+        starCount = (await (await fetch("https://api.github.com/repos/languageXchange/languageXchange?page=$i&per_page=100")).json()).stargazers_count
+    })
+
+</script>
+<div class:mtop={starCount > 0} class="star-count">
+    <a class="star" href="https://github.com/languageXchange/languageXchange/stargazers">
+        <Star />
+    </a>
+    {#if starCount > 0}
+    <small>{starCount}</small>
+    {/if}
+</div>
+<style lang="scss">
+    .star-count {
+        .mtop {
+            margin-top: 10px;
+        }
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        small {
+            color: var(--color--text);
+        }
+
+        .star {
+            color: var(--color--text);
+            height: 24px;
+        }
+    }
+</style>
