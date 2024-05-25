67f8938773227d2fbe13371ea795498e3dc783e8
xue <behicsakar@gmail.com>
2024-05-25 07:15:46 +0300

    chore: remove commented out code

--- src/lib/components/molecules/AnnouncementModal.svelte
+++ src/lib/components/molecules/AnnouncementModal.svelte
@@ -26,20 +26,6 @@
 			</div>
 		</div>
 	</div>
-
-	<!--	<div class="modal" transition:slide={{ duration: 500 }}>-->
-	<!--		<div class="modal-content">-->
-	<!--			<button class="close-button" on:click={() => (show = false)}>X</button>-->
-	<!--			<div class="content">-->
-	<!--				<h4>🚀 Excited to announce the launch of our innovative 'Learn to Earn' Token Project</h4>-->
-	<!--				<p>-->
-	<!--					🥇 Get ready to revolutionize language learning and earn reward <SparklingHighlight-->
-	<!--						><a href="http://token.langx.io" target="_blank">LangX Token</a></SparklingHighlight-->
-	<!--					> while you improve your skills!-->
-	<!--				</p>-->
-	<!--			</div>-->
-	<!--		</div>-->
-	<!--	</div>-->
 {/if}
 
 <style lang="scss">
@@ -122,38 +108,4 @@
 			}
 		}
 	}
-
-	//.modal {
-	//	position: relative;
-	//	z-index: 9999;
-	//	width: 100%;
-	//	background-color: var(--color--secondary);
-	//}
-	//
-	//.modal-content {
-	//	position: relative;
-	//	margin: 0 auto;
-	//	padding: 20px;
-	//	width: 90%;
-	//}
-	//
-	//a {
-	//	text-decoration-color: var(--color--text);
-	//}
-	//
-	//.content {
-	//	padding-right: 50px;
-	//}
-	//
-	//.close-button {
-	//	position: absolute;
-	//	right: 10px;
-	//	top: 50%;
-	//	transform: translateY(-50%);
-	//	border: none;
-	//	background: none;
-	//	font-size: 1.5em;
-	//	cursor: pointer;
-	//	color: var(--color--text);
-	//}
 </style>
